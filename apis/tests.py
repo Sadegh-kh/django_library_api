421053ecf836869444af2472352e33b6fa2c1a69
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-08-26 13:13:03 +0330

    init test for book list api

--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+from books.models import Book
+
+
+class BookApiTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="Django for APIs",
+            subtitle="Build web APIs with Python and Django",
+            author="William S. Vincent",
+        )
+
+    def test_book_list_api(self):
+        response = self.client.get(reverse("apis:book_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, self.book)
