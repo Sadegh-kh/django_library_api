9a7bc1a9f243ec900471fb8979bc0633099e4a10
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-08-26 12:01:59 +0330

    init book list page

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+from . import models
+
+
+class BookList(ListView):
+    model = models.Book
+    template_name = "books/book_list.html"
+    context_object_name = "books"
